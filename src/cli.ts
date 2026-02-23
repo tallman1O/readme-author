6b771f225a17ff4fc89784fd2e07010ebb390987
Mehul Uttam <mehulsavio@gmail.com>
2026-02-23 17:17:54 +0530

    Enhance CLI output with styled text using chalk

--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import chalk from 'chalk';
 
 const program = new Command();
 
-console.log("Hello world");
+const log = console.log;
+
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
 program
   .name("readme-gen")
   .description("Auto generate README.md from project")
